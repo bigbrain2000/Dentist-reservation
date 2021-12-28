46a6c23ed9ac46d2044c95e2fc3cb160ff9498f4
Roncea Marius-Alexandru <ronceamarius84@yahoo.com>
2021-12-28 11:02:25 +0200

    Dentist can logout
    I created the functionality for Dentist to logout from app.

---
+++ src/main/java/controllers/LoginPageInterface.java
@@ -0,0 +1,7 @@
+package controllers;
+
+import org.jetbrains.annotations.NotNull;
+
+public interface LoginPageInterface {
+    static void LoadLoginFXML(@NotNull javafx.event.ActionEvent event){}
+}
